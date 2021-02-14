93127ca2edfdf25b821b102ad369aa131f24ed48
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-02-14 16:45:18 +0900

    # note

--- src/form/input.php
+++ src/form/input.php
@@ -6,7 +6,17 @@ if (!empty($_POST["your_name"])){
     echo "<pre>";
     var_dump($_POST);
     echo "</pre>";
-}
+};
+
+/* 入力、確認、完了：input.php, confirm.php, thanks.php */
+/* 今回は "input.php" のみで表現する */
+$pageFlag = 0;
+if(!empty($_POST["btn_confirm"])){
+    $pageFlag = 1;
+};
+if(!empty($_POST["btn_submit"])){
+    $pageFlag = 2;
+};
 
 ?>
 
@@ -15,15 +25,37 @@ if (!empty($_POST["your_name"])){
 <head></head>
 <body>
 
+<!-- 確認画面 -->
+<?php if($pageFlag === 1) : ?>
+<form method="POST" action="input.php">
+氏名
+<?php echo $_POST["your_name"]; ?>
+<br>
+メールアドレス
+<?php echo $_POST["email"]; ?>
+<br>
+<input type="submit" name="btn_submit" value="送信する">
+<input type="hidden" name="your_name" value="<?php echo $_POST["your_name"]; ?>">
+<input type="hidden" name="email" value="<?php echo $_POST["email"]; ?>">
+<?php endif; ?>
+
+<!-- 完了画面 -->
+<?php if($pageFlag === 2) : ?>
+送信が完了しました
+<?php endif; ?>
+
+<!-- 入力画面 -->
+<?php if($pageFlag === 0) : ?>
 <form method="POST" action="input.php">
 氏名
 <input type="text" name="your_name">
 <br>
-<input type="checkbox" name="sports[]" value="野球">野球
-<input type="checkbox" name="sports[]" value="サッカー">サッカー
-<input type="checkbox" name="sports[]" value="バスケ">バスケ
-<input type="submit" value="送信">
+メールアドレス
+<input type="email" name="email">
+<br>
+<input type="submit" name="btn_confirm" value="確認する">
 </form>
+<?php endif; ?>
 
 </body>
 </html>
