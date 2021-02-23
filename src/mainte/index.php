af978b90649f96b96ad198676b0cbefd328a8fc0
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-02-23 19:00:11 +0900

    # note

--- src/mainte/index.php
+++ src/mainte/index.php
@@ -25,5 +25,19 @@ $result = $stmt->fetchall();
 echo '<pre>';
 var_dump($result);
 echo '</pre>';
+
+// Transaction まとまって処理 beginTransaction, commit, rollback
+// ex)銀行 残高を確認->Aさんから引き落とし->Bさんに振込
+$pdo->beginTransaction();
+try {
+  // sql処理
+  $stmt = $pdo->prepare($sql); // prepared statememt
+  $stmt->bindValue('id', $selectId, PDO::PARAM_INT); // 紐付け
+  $stmt->execute(); //実行
+
+  $pdo->commit();
+} catch (PDOException $e) {
+  $pdo->rollback(); // 更新のキャンセル
+}
 ?>
 <!-- http://localhost:8888/2021_PHP-Laravel-HTTP-PDO-composer-/src/mainte/index.php -->
